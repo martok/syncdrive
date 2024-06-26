e670dad157bfbb78262cb3e69ed6942d558e7f48
Martok <martok@martoks-place.de>
2024-06-26 23:20:08 +0200

    dep: internalize all dependencies

---
+++ app/build-libs.php
@@ -0,0 +1,34 @@
+<?php
+
+$outDir = realpath(__DIR__ . '/../public/static/lib');
+
+function getAndMerge(string $fileName, array $sourceUrls): void
+{
+    $contents = [];
+
+    foreach($sourceUrls as $sourceUrl) {
+        echo "Fetching $sourceUrl...\n";
+        $src = file_get_contents($sourceUrl);
+        $ts = gmdate(DATE_RFC3339);
+        $contents[] = "/* $sourceUrl fetched at $ts */";
+        $contents[] = $src;
+        $contents[] = "";
+    }
+
+    @mkdir(dirname($fileName), recursive: true);
+    file_put_contents($fileName, join("\n", $contents));
+    echo "Created $fileName...\n";
+}
+
+getAndMerge($outDir . '/merged.css', [
+    'https://cdn.jsdelivr.net/npm/uikit@3.17.4/dist/css/uikit.min.css',
+    'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
+]);
+
+getAndMerge($outDir . '/merged.js', [
+    'https://cdn.jsdelivr.net/npm/uikit@3.17.4/dist/js/uikit.min.js',
+    'https://cdn.jsdelivr.net/npm/uikit@3.17.4/dist/js/uikit-icons.min.js',
+    'https://cdn.jsdelivr.net/npm/jquery@3.7/dist/jquery.min.js',
+    'https://cdn.jsdelivr.net/npm/timeago@1.6.7/jquery.timeago.min.js',
+    'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js',
+]);
